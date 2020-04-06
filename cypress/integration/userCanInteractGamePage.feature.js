e497abd72fe81dea3d4d07c39c5f7b8af14671e1
Carlos <carlos.delgado@hotmail.it>
2020-04-06 21:00:50 +0200

    fixing secutiry error

--- cypress/integration/userCanInteractGamePage.feature.js
+++ cypress/integration/userCanInteractGamePage.feature.js
@@ -21,8 +21,52 @@ describe("Page can show results", () => {
     it('user picks paper equals a lost', () => {
       cy.get("#SCISSOR").click();
       cy.get("#result").should('contain','Computer chose rock you lost')
-
+    
   
   });
+  })
+
+
+  describe('when computer picks paper', () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000", {
+        onBeforeLoad: winner => {
+          cy.stub(winner.Math, 'floor').returns(1)
+        }
+      })
+     })
+    it('user picks rock equals a lost', () => {
+      cy.get("#ROCK").click();
+      cy.get("#result").should('contain', 'Computer chose paper you lost')
+      })
+
+      it('user picks paper equals a draw', () => {
+        cy.get("#PAPER").click();
+        cy.get("#result").should('contain', 'tied')
+        });
+
+      it('user picks scissor equals a draw', () => {
+        cy.get("#SCISSOR").click();
+        cy.get("#result").should('contain', 'Computer chose paper you won')
+
+      })
+
+
+
+        describe('when computer picks scissor', () => {
+          beforeEach(() => {
+            cy.visit("http://localhost:3000", {
+              onBeforeLoad: winner => {
+                cy.stub(winner.Math, 'floor').returns(2)
+              }
+          })
+        })
+
+      it('user picks rock equals a lost', () => {
+        cy.get("#ROCK").click();
+        cy.get("#result").should('contain', 'Computer chose scissor you lost')
+        })
+      
+      })
   })
 })
